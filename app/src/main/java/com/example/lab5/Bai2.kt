da44cc69582f5adbfef243630e9f6642ef23253f
Vanh2k20 <avan81837@gmail.com>
2024-05-30 11:06:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab5/Bai2.kt
@@ -0,0 +1,68 @@
+package com.example.lab5
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun Bai2 (navController: NavController){
+    LightSwitch()
+}
+@Composable
+fun LightSwitch(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var isChecked by remember{ mutableStateOf(false) }
+        if (isChecked){
+            Image(
+                painter = painterResource(id = R.drawable.bulb_on),
+                contentDescription = "on",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth)
+        }else{
+            Image(
+                painter = painterResource(id = R.drawable.bulb_off),
+                contentDescription = "off",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth)
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Switch(
+            checked = isChecked,
+            onCheckedChange = {isChecked = it},
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = Color.Green,
+                uncheckedThumbColor = Color.Gray,
+                checkedTrackColor = Color.Green.copy(0.5f),
+                uncheckedTrackColor = Color.Gray.copy(0.5f),
+                checkedBorderColor = Color.Green.copy(0.75f)
+            ))
+    }
+}
+
